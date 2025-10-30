0d13e26d53181a1c196d9252f182864247f53064
Holger Borum <holger.stadel.borum@gmail.com>
2025-10-30 20:59:36 +0100

    TTS second attempt

---
+++ tabletop.lua
@@ -0,0 +1,74 @@
+
+function onLoad()
+    
+    local deckJSON = [[
+        {"Name":"Deck","Transform":{"posX":0,"posY":2,"posZ":0,"rotX":0,"rotY":180,"rotZ":180,"scaleX":1.3,"scaleY":1.3,"scaleZ":1.3},"CustomDeck":{"1":{"FaceURL":"C:\\projects\\games\\autochess\\Cardio\\bin\\Debug\\net9.0\\resources\\out\\deck\\digital\\raging-bull.png","BackURL":"C:\\projects\\games\\autochess\\Cardio\\bin\\Debug\\net9.0\\resources\\raging-bull.png","NumWidth":10,"NumHeight":7,"BackIsHidden":false}},"DeckIDs":[101,102,103,104,105,106,107],"ContainedObjects":[{"Name":"Card","CardID":101,"Nickname":"Slash"},{"Name":"Card","CardID":102,"Nickname":"Right Horn"},{"Name":"Card","CardID":103,"Nickname":"Left Horn"},{"Name":"Card","CardID":104,"Nickname":"Thunderous Hooves"},{"Name":"Card","CardID":105,"Nickname":"Gore"},{"Name":"Card","CardID":106,"Nickname":"Gallop"},{"Name":"Card","CardID":107,"Nickname":"Bull Rush"}]}
+    ]]
+    spawnObjectJSON({json = deckJSON, position = {-17, 2, 10}})
+
+    local deckJSON = [[
+        {"Name":"Deck","Transform":{"posX":0,"posY":2,"posZ":0,"rotX":0,"rotY":180,"rotZ":180,"scaleX":1.3,"scaleY":1.3,"scaleZ":1.3},"CustomDeck":{"1":{"FaceURL":"C:\\projects\\games\\autochess\\Cardio\\bin\\Debug\\net9.0\\resources\\out\\deck\\digital\\swarm.png","BackURL":"C:\\projects\\games\\autochess\\Cardio\\bin\\Debug\\net9.0\\resources\\swarm.png","NumWidth":10,"NumHeight":7,"BackIsHidden":false}},"DeckIDs":[101,102,103,104,105,106,107,108,109],"ContainedObjects":[{"Name":"Card","CardID":101,"Nickname":"Loot"},{"Name":"Card","CardID":102,"Nickname":"Twisting Vines"},{"Name":"Card","CardID":103,"Nickname":"Root"},{"Name":"Card","CardID":104,"Nickname":"Toxic Spores"},{"Name":"Card","CardID":105,"Nickname":"Spores"},{"Name":"Card","CardID":106,"Nickname":"Harvest"},{"Name":"Card","CardID":107,"Nickname":"Whip"},{"Name":"Card","CardID":108,"Nickname":"Blossom"},{"Name":"Card","CardID":109,"Nickname":"Seed"}]}
+    ]]
+    spawnObjectJSON({json = deckJSON, position = {-14, 2, 10}})
+
+    local deckJSON = [[
+        {"Name":"Deck","Transform":{"posX":0,"posY":2,"posZ":0,"rotX":0,"rotY":180,"rotZ":180,"scaleX":1.3,"scaleY":1.3,"scaleZ":1.3},"CustomDeck":{"1":{"FaceURL":"C:\\projects\\games\\autochess\\Cardio\\bin\\Debug\\net9.0\\resources\\out\\deck\\digital\\dragon.png","BackURL":"C:\\projects\\games\\autochess\\Cardio\\bin\\Debug\\net9.0\\resources\\dragon.png","NumWidth":10,"NumHeight":7,"BackIsHidden":false}},"DeckIDs":[101,102,103,104,105,106,107,108,109,110],"ContainedObjects":[{"Name":"Card","CardID":101,"Nickname":"Scorch"},{"Name":"Card","CardID":102,"Nickname":"Torch"},{"Name":"Card","CardID":103,"Nickname":"Blasting Heat"},{"Name":"Card","CardID":104,"Nickname":"Cough"},{"Name":"Card","CardID":105,"Nickname":"Check"},{"Name":"Card","CardID":106,"Nickname":"Mate"},{"Name":"Card","CardID":107,"Nickname":"Tail Swipe"},{"Name":"Card","CardID":108,"Nickname":"Depth First Breath"},{"Name":"Card","CardID":109,"Nickname":"Right Breath"},{"Name":"Card","CardID":110,"Nickname":"Love"}]}
+    ]]
+    spawnObjectJSON({json = deckJSON, position = {-11, 2, 10}})
+
+    local deckJSON = [[
+        {"Name":"Deck","Transform":{"posX":0,"posY":2,"posZ":0,"rotX":0,"rotY":180,"rotZ":180,"scaleX":1.3,"scaleY":1.3,"scaleZ":1.3},"CustomDeck":{"1":{"FaceURL":"C:\\projects\\games\\autochess\\Cardio\\bin\\Debug\\net9.0\\resources\\out\\deck\\digital\\gardener.png","BackURL":"C:\\projects\\games\\autochess\\Cardio\\bin\\Debug\\net9.0\\resources\\gardener.png","NumWidth":10,"NumHeight":7,"BackIsHidden":false}},"DeckIDs":[],"ContainedObjects":[]}
+    ]]
+    spawnObjectJSON({json = deckJSON, position = {-8, 2, 10}})
+
+    local deckJSON = [[
+        {"Name":"Deck","Transform":{"posX":0,"posY":2,"posZ":0,"rotX":0,"rotY":180,"rotZ":180,"scaleX":1.3,"scaleY":1.3,"scaleZ":1.3},"CustomDeck":{"1":{"FaceURL":"C:\\projects\\games\\autochess\\Cardio\\bin\\Debug\\net9.0\\resources\\out\\deck\\digital\\golem.png","BackURL":"C:\\projects\\games\\autochess\\Cardio\\bin\\Debug\\net9.0\\resources\\golem.png","NumWidth":10,"NumHeight":7,"BackIsHidden":false}},"DeckIDs":[101,102,103,104,105,106,107,108,109],"ContainedObjects":[{"Name":"Card","CardID":101,"Nickname":"Stalag Might"},{"Name":"Card","CardID":102,"Nickname":"Groundpound"},{"Name":"Card","CardID":103,"Nickname":"Rock Throw"},{"Name":"Card","CardID":104,"Nickname":"Cave-in"},{"Name":"Card","CardID":105,"Nickname":"Mud bath"},{"Name":"Card","CardID":106,"Nickname":"Stalag Tight"},{"Name":"Card","CardID":107,"Nickname":"Quake"},{"Name":"Card","CardID":108,"Nickname":"Rolling earth"},{"Name":"Card","CardID":109,"Nickname":"Rock Blast"}]}
+    ]]
+    spawnObjectJSON({json = deckJSON, position = {-5, 2, 10}})
+
+    local deckJSON = [[
+        {"Name":"Deck","Transform":{"posX":0,"posY":2,"posZ":0,"rotX":0,"rotY":180,"rotZ":180,"scaleX":1.3,"scaleY":1.3,"scaleZ":1.3},"CustomDeck":{"1":{"FaceURL":"C:\\projects\\games\\autochess\\Cardio\\bin\\Debug\\net9.0\\resources\\out\\deck\\digital\\cutthroat.png","BackURL":"C:\\projects\\games\\autochess\\Cardio\\bin\\Debug\\net9.0\\resources\\cutthroat.png","NumWidth":10,"NumHeight":7,"BackIsHidden":false}},"DeckIDs":[101,102,103],"ContainedObjects":[{"Name":"Card","CardID":101,"Nickname":"Slash"},{"Name":"Card","CardID":102,"Nickname":"Eviscerate"},{"Name":"Card","CardID":103,"Nickname":"Cloak"}]}
+    ]]
+    spawnObjectJSON({json = deckJSON, position = {-2, 2, 10}})
+
+    local deckJSON = [[
+        {"Name":"Deck","Transform":{"posX":0,"posY":2,"posZ":0,"rotX":0,"rotY":180,"rotZ":180,"scaleX":1.3,"scaleY":1.3,"scaleZ":1.3},"CustomDeck":{"1":{"FaceURL":"C:\\projects\\games\\autochess\\Cardio\\bin\\Debug\\net9.0\\resources\\out\\deck\\digital\\ecclesiarch.png","BackURL":"C:\\projects\\games\\autochess\\Cardio\\bin\\Debug\\net9.0\\resources\\ecclesiarch.png","NumWidth":10,"NumHeight":7,"BackIsHidden":false}},"DeckIDs":[101,102,103],"ContainedObjects":[{"Name":"Card","CardID":101,"Nickname":"Heal"},{"Name":"Card","CardID":102,"Nickname":"Bad Touch"},{"Name":"Card","CardID":103,"Nickname":"Teleport"}]}
+    ]]
+    spawnObjectJSON({json = deckJSON, position = {1, 2, 10}})
+
+    local deckJSON = [[
+        {"Name":"Deck","Transform":{"posX":0,"posY":2,"posZ":0,"rotX":0,"rotY":180,"rotZ":180,"scaleX":1.3,"scaleY":1.3,"scaleZ":1.3},"CustomDeck":{"1":{"FaceURL":"C:\\projects\\games\\autochess\\Cardio\\bin\\Debug\\net9.0\\resources\\out\\deck\\digital\\engine.png","BackURL":"C:\\projects\\games\\autochess\\Cardio\\bin\\Debug\\net9.0\\resources\\engine.png","NumWidth":10,"NumHeight":7,"BackIsHidden":false}},"DeckIDs":[101,102,103],"ContainedObjects":[{"Name":"Card","CardID":101,"Nickname":"Heal"},{"Name":"Card","CardID":102,"Nickname":"Broadside"},{"Name":"Card","CardID":103,"Nickname":"U-turn"}]}
+    ]]
+    spawnObjectJSON({json = deckJSON, position = {4, 2, 10}})
+
+    local deckJSON = [[
+        {"Name":"Deck","Transform":{"posX":0,"posY":2,"posZ":0,"rotX":0,"rotY":180,"rotZ":180,"scaleX":1.3,"scaleY":1.3,"scaleZ":1.3},"CustomDeck":{"1":{"FaceURL":"C:\\projects\\games\\autochess\\Cardio\\bin\\Debug\\net9.0\\resources\\out\\deck\\digital\\evoker.png","BackURL":"C:\\projects\\games\\autochess\\Cardio\\bin\\Debug\\net9.0\\resources\\evoker.png","NumWidth":10,"NumHeight":7,"BackIsHidden":false}},"DeckIDs":[101,102,103],"ContainedObjects":[{"Name":"Card","CardID":101,"Nickname":"Immolation"},{"Name":"Card","CardID":102,"Nickname":"Cauterize"},{"Name":"Card","CardID":103,"Nickname":"Death Ray"}]}
+    ]]
+    spawnObjectJSON({json = deckJSON, position = {7, 2, 10}})
+
+    local deckJSON = [[
+        {"Name":"Deck","Transform":{"posX":0,"posY":2,"posZ":0,"rotX":0,"rotY":180,"rotZ":180,"scaleX":1.3,"scaleY":1.3,"scaleZ":1.3},"CustomDeck":{"1":{"FaceURL":"C:\\projects\\games\\autochess\\Cardio\\bin\\Debug\\net9.0\\resources\\out\\deck\\digital\\mystic.png","BackURL":"C:\\projects\\games\\autochess\\Cardio\\bin\\Debug\\net9.0\\resources\\mystic.png","NumWidth":10,"NumHeight":7,"BackIsHidden":false}},"DeckIDs":[101,102,103,104,105,106],"ContainedObjects":[{"Name":"Card","CardID":101,"Nickname":"Swapper"},{"Name":"Card","CardID":102,"Nickname":"Hurt"},{"Name":"Card","CardID":103,"Nickname":"Hedgehog"},{"Name":"Card","CardID":104,"Nickname":"Yuch"},{"Name":"Card","CardID":105,"Nickname":"Shoot"},{"Name":"Card","CardID":106,"Nickname":"Help"}]}
+    ]]
+    spawnObjectJSON({json = deckJSON, position = {10, 2, 10}})
+
+    local deckJSON = [[
+        {"Name":"Deck","Transform":{"posX":0,"posY":2,"posZ":0,"rotX":0,"rotY":180,"rotZ":180,"scaleX":1.3,"scaleY":1.3,"scaleZ":1.3},"CustomDeck":{"1":{"FaceURL":"C:\\projects\\games\\autochess\\Cardio\\bin\\Debug\\net9.0\\resources\\out\\deck\\digital\\saurian.png","BackURL":"C:\\projects\\games\\autochess\\Cardio\\bin\\Debug\\net9.0\\resources\\saurian.png","NumWidth":10,"NumHeight":7,"BackIsHidden":false}},"DeckIDs":[101,102,103],"ContainedObjects":[{"Name":"Card","CardID":101,"Nickname":"Swipe"},{"Name":"Card","CardID":102,"Nickname":"Tail whip"},{"Name":"Card","CardID":103,"Nickname":"Toxic spines"}]}
+    ]]
+    spawnObjectJSON({json = deckJSON, position = {13, 2, 10}})
+
+    local deckJSON = [[
+        {"Name":"Deck","Transform":{"posX":0,"posY":2,"posZ":0,"rotX":0,"rotY":180,"rotZ":180,"scaleX":1.3,"scaleY":1.3,"scaleZ":1.3},"CustomDeck":{"1":{"FaceURL":"C:\\projects\\games\\autochess\\Cardio\\bin\\Debug\\net9.0\\resources\\out\\deck\\digital\\stalker.png","BackURL":"C:\\projects\\games\\autochess\\Cardio\\bin\\Debug\\net9.0\\resources\\stalker.png","NumWidth":10,"NumHeight":7,"BackIsHidden":false}},"DeckIDs":[101,102,103],"ContainedObjects":[{"Name":"Card","CardID":101,"Nickname":"Loose"},{"Name":"Card","CardID":102,"Nickname":"Scoot 'n shoot"},{"Name":"Card","CardID":103,"Nickname":"Pushing shot"}]}
+    ]]
+    spawnObjectJSON({json = deckJSON, position = {16, 2, 10}})
+
+    local deckJSON = [[
+        {"Name":"Deck","Transform":{"posX":0,"posY":2,"posZ":0,"rotX":0,"rotY":180,"rotZ":180,"scaleX":1.3,"scaleY":1.3,"scaleZ":1.3},"CustomDeck":{"1":{"FaceURL":"C:\\projects\\games\\autochess\\Cardio\\bin\\Debug\\net9.0\\resources\\out\\deck\\digital\\vanguard.png","BackURL":"C:\\projects\\games\\autochess\\Cardio\\bin\\Debug\\net9.0\\resources\\vanguard.png","NumWidth":10,"NumHeight":7,"BackIsHidden":false}},"DeckIDs":[101,102,103],"ContainedObjects":[{"Name":"Card","CardID":101,"Nickname":"Armor shatter"},{"Name":"Card","CardID":102,"Nickname":"Sword and board"},{"Name":"Card","CardID":103,"Nickname":"Sacrifice"}]}
+    ]]
+    spawnObjectJSON({json = deckJSON, position = {19, 2, 10}})
+
+    local deckJSON = [[
+        {"Name":"Deck","Transform":{"posX":0,"posY":2,"posZ":0,"rotX":0,"rotY":180,"rotZ":180,"scaleX":1.3,"scaleY":1.3,"scaleZ":1.3},"CustomDeck":{"1":{"FaceURL":"C:\\projects\\games\\autochess\\Cardio\\bin\\Debug\\net9.0\\resources\\out\\deck\\digital\\draft.png","BackURL":"C:\\projects\\games\\autochess\\Cardio\\bin\\Debug\\net9.0\\resources\\draft.png","NumWidth":10,"NumHeight":7,"BackIsHidden":false}},"DeckIDs":[101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,123,124,125,126,127,128,129,130,131,132,133,134,135,136,137,138,139,140,141,142,143,144,145,146,147,148,149,150,151,152,153,154,155,156,157,158,159],"ContainedObjects":[{"Name":"Card","CardID":101,"Nickname":"Fan of knives"},{"Name":"Card","CardID":102,"Nickname":"Delay"},{"Name":"Card","CardID":103,"Nickname":"Leap frog"},{"Name":"Card","CardID":104,"Nickname":"Shadows of Doubt"},{"Name":"Card","CardID":105,"Nickname":"Ice Cube"},{"Name":"Card","CardID":106,"Nickname":"Tunnel"},{"Name":"Card","CardID":107,"Nickname":"Restore"},{"Name":"Card","CardID":108,"Nickname":"Fire walk"},{"Name":"Card","CardID":109,"Nickname":"Flee"},{"Name":"Card","CardID":110,"Nickname":"Poisonous Cloud"},{"Name":"Card","CardID":111,"Nickname":"Blow Dart"},{"Name":"Card","CardID":112,"Nickname":"Step Back"},{"Name":"Card","CardID":113,"Nickname":"Revenge"},{"Name":"Card","CardID":114,"Nickname":"Safe for later"},{"Name":"Card","CardID":115,"Nickname":"Repeat action"},{"Name":"Card","CardID":116,"Nickname":"Temper"},{"Name":"Card","CardID":117,"Nickname":"Flaming Arrow"},{"Name":"Card","CardID":118,"Nickname":"Bad meat"},{"Name":"Card","CardID":119,"Nickname":"Burning sacrifice"},{"Name":"Card","CardID":120,"Nickname":"Unexpected angle"},{"Name":"Card","CardID":121,"Nickname":"Keep the combo"},{"Name":"Card","CardID":122,"Nickname":"Reach"},{"Name":"Card","CardID":123,"Nickname":"Provide Toxicity"},{"Name":"Card","CardID":124,"Nickname":"Venomous arrow"},{"Name":"Card","CardID":125,"Nickname":"Thrown Shield"},{"Name":"Card","CardID":126,"Nickname":"Magic missiles"},{"Name":"Card","CardID":127,"Nickname":"Release Steam"},{"Name":"Card","CardID":128,"Nickname":"Siphon"},{"Name":"Card","CardID":129,"Nickname":"Black Hole"},{"Name":"Card","CardID":130,"Nickname":"Inspire"},{"Name":"Card","CardID":131,"Nickname":"Bonfire"},{"Name":"Card","CardID":132,"Nickname":"Flaming sword"},{"Name":"Card","CardID":133,"Nickname":"Envenomed Weapons"},{"Name":"Card","CardID":134,"Nickname":"Heat"},{"Name":"Card","CardID":135,"Nickname":"Deep focus"},{"Name":"Card","CardID":136,"Nickname":"Hook"},{"Name":"Card","CardID":137,"Nickname":"Lead"},{"Name":"Card","CardID":138,"Nickname":"Protect"},{"Name":"Card","CardID":139,"Nickname":"Spear"},{"Name":"Card","CardID":140,"Nickname":"Javelin"},{"Name":"Card","CardID":141,"Nickname":"Jumping attack"},{"Name":"Card","CardID":142,"Nickname":"Taunt"},{"Name":"Card","CardID":143,"Nickname":"Combine shoot"},{"Name":"Card","CardID":144,"Nickname":"Reflect a lot"},{"Name":"Card","CardID":145,"Nickname":"Av"},{"Name":"Card","CardID":146,"Nickname":"Aim"},{"Name":"Card","CardID":147,"Nickname":"Tinder box"},{"Name":"Card","CardID":148,"Nickname":"Oil"},{"Name":"Card","CardID":149,"Nickname":"Apply poison"},{"Name":"Card","CardID":150,"Nickname":"Combine Hit"},{"Name":"Card","CardID":151,"Nickname":"Combine Heal"},{"Name":"Card","CardID":152,"Nickname":"Combine Defend"},{"Name":"Card","CardID":153,"Nickname":"Combo shoot"},{"Name":"Card","CardID":154,"Nickname":"Fangs"},{"Name":"Card","CardID":155,"Nickname":"Combo hit"},{"Name":"Card","CardID":156,"Nickname":"Combo heal"},{"Name":"Card","CardID":157,"Nickname":"Combo defend"},{"Name":"Card","CardID":158,"Nickname":"Throw"},{"Name":"Card","CardID":159,"Nickname":"Salt to wound"}]}
+    ]]
+    spawnObjectJSON({json = deckJSON, position = {22, 2, 10}})
+
+end
--- tech-trees/topdf.bat
+++
@@ -1,4 +0,0 @@
-  dot -Tpdf cleric.dot -o cleric.pdf
-dot -Tpdf hunter.dot -o hunter.pdf
-dot -Tpdf warrior.dot -o warrior.pdf
-dot -Tpdf rogue.dot -o rogue.pdf
